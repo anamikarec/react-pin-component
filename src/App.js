cb84860fedad06e6c7cddd562a02ef6bb045e686
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-28 23:32:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import PinInput from "./Pin/PinInput";
+import "./styles.css";
+
+export default function App() {
+  const [state, setState] = useState("");
+  return (
+    <div className="App">
+      <h1>Custom Component</h1>
+      <PinInput onChange={(pin) => setState(pin)} />
+      <hr />
+      <div>
+        {state.length === 5 ? (
+          <div style={{ color: "green" }}>{state}</div>
+        ) : (
+          <div>{state}</div>
+        )}
+      </div>
+    </div>
+  );
+}
